072b9dcd34a401123ee6c686ff52f8a240ddfb7a
TDxBruuno <eldani16804@gmail.com>
2023-09-11 09:10:56 -0300

    Hecho

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -6,12 +6,13 @@
 
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Full_GRASP_And_SOLID.Library
 {
     public class Recipe
     {
-        private ArrayList steps = new ArrayList();
+        internal List<Step> steps = new List<Step>();
 
         public Product FinalProduct { get; set; }
 
@@ -29,6 +30,26 @@ namespace Full_GRASP_And_SOLID.Library
         {
             Console.WriteLine($"Receta de {this.FinalProduct.Description}:");
             foreach (Step step in this.steps)
+            {
+                Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
+                    $"usando '{step.Equipment.Description}' durante {step.Time}");
+            }
+        }
+    }
+
+     public interface IRecipePrinter
+    {
+        void PrintRecipe(Recipe recipe);
+    }
+
+    // Creamos una implementación de la interfaz IRecipePrinter que imprime en la consola.
+    //para implementar esta funcionalidad utilicé el principio SOLID de "o" (Principio de Open/Closed)
+    public class ConsolePrinter : IRecipePrinter
+    {
+        public void PrintRecipe(Recipe recipe)
+        {
+            Console.WriteLine($"Receta de {recipe.FinalProduct.Description}:");
+            foreach (Step step in recipe.steps)
             {
                 Console.WriteLine($"{step.Quantity} de '{step.Input.Description}' " +
                     $"usando '{step.Equipment.Description}' durante {step.Time}");
